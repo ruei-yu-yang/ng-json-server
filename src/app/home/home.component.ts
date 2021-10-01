07ceec9f67ee1dde6b2489903bfbeb966cbc3b75
Edward Yang <Edward_Yang@asus.com>
2021-10-01 14:49:38 +0800

    init project

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IUser } from '../core/interfaces/user.interface';
+import { getUsers } from '../store/app.actions';
+import { AppState } from '../store/app.reducer';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  users$: Observable<IUser[]>;
+  constructor(
+    private store: Store<AppState>
+  ) { }
+
+  ngOnInit() {
+    this.store.dispatch(getUsers())
+  }
+
+
+  ngOnDestroy() { }
+}
